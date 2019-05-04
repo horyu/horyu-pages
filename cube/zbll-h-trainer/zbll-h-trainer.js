6aefc1233f938fafe67b949f458a7685f251d9c4
horyu <horyu@users.noreply.github.com>
2019-05-04 18:09:58 +0900

    add zbll-h-trainer

---
+++ cube/zbll-h-trainer/zbll-h-trainer.js
@@ -0,0 +1,123 @@
+"use strict";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class ConfigMaker {
+  constructor() {
+    this.halgs = {
+      tate1: [
+        "y R U' L U R' U2 L' U L U L' U' L U L'",
+        "y L' U R' U' L U2 R U' R' U' R U R' U' R",
+        "y R U R' U R U2 R' F R U' R' U' R U2 R' U' F'",
+        "y2 R2 D R' U2 R D' R U' R2 U' R2 U2 R",
+        "L2 D' L U2 L' D L' U L2 U L2 U2 L'",
+        "y2 L' U' L U' L' U' R U' L U R'",
+        "R U R' U R U L' U R' U' L",
+        "L U L' U L' U' L2 U' L2 U' R U' L U R'",
+        "y2 R' U' R U' R U R2 U R2 U L' U R' U' L",
+        "y R U' L' U R2 U' R L U2 R' U' R",
+        "y L' U R U' L2 U L' R' U2 L U L'",
+        "D R' U' R D' R U' R' U2 R U2 R U R U' R2",
+      ],
+      tate2: [
+        "B' R U' R' U2 R2 U R' U2 R U R2 U' B",
+        "B L' U L U2 L2 U' L U2 L' U' L2 U B'",
+        "x' U' R U' R' U R' F2 R U' R U R' U x",
+        "y' R2 F R F' R' U2 R' U2 R U2 x' U' R2 U R",
+        "y L2 F' L' F L U2 L U2 L' U2 x' U L2 U' L'",
+        "y R U' R2 U' F2 U' R2 U R2 U F2 R2 U R'",
+        "y F R U R' U' R U R' U' R U R' U' F'",
+        "y' L' U L2 U F2 U L2 U' L2 U' F2 L2 U' L",
+      ],
+      yoko1: [
+        "x' M' U' R U' R' U R U2 L' U R' U2 R",
+        "x' M' U L' U L U' L' U2 R U' L U2 L'",
+        "y' F R U R' U' R' F' U2 R U R' U R2 U2 R'",
+        "R U2 R' U' R U' R D' R U' R' D R U R",
+        "L' U2 L U L' U L' D L' U L D' L' U' L'",
+        "R U' L2 D' L U2 L' D L U' R' U' L",
+        "L' U R2 D R' U2 R D' R' U L U R'",
+        "F' B L2 B' L2 U L2 U L2 U' L2 F ",
+        "F B' R2 B R2 U' R2 U' R2 U R2 F'",
+        "y2 F R U' R' U R U2 R' U' R U R' U' F'",
+        "F' U L' U2 L U2 L' U L U L' U' L U' F",
+        "F U' R U2 R' U2 R U' R' U' R U R' U F'",
+      ],
+      yoko2: [
+        "R U R' U R U' R' U R U2 R'",
+        "L' U' L U' L' U L U' L' U2 L",
+        "y L' U2 L U L' U' L U L' U L",
+        "y' R U2 R' U' R U R' U' R U' R'",
+        "y' R' U2 R U R' U R U R U R' U R U2 R'",
+        "y L U2 L' U' L U' L' U' L' U' L U' L' U2 L",
+        "R U R' U R U2 R' U' R' U2 R U R' U R",
+        "y R U R2 U' R2 U' R U2 R U2 R U' R2 U' R2 U R",
+      ],
+    };
+    this.targets = [];
+    this.cn;
+  }
+  config() {
+    this.targets = [];
+    for (let input of document.getElementsByClassName('config-alg')) {
+      if (input.checked) {
+        this.targets = this.targets.concat(this.halgs[input.id]);
+      }
+    }
+    this.cn = document.getElementById('cn').checked;
+  }
+  make() {
+    const auf = ["", "U ", "U2 ", "U' "][getRandomInt(4)];
+    const alg = this.targets[getRandomInt(this.targets.length)];
+    let conf = `alg=${auf + alg}|colored=U*|hover=none`;
+    if (this.cn) {
+      conf += this._colors();
+    }
+    return conf;
+  }
+  _colors() {
+    let c = { F: 'b', B: 'g', U: 'r', D: 'o', R: 'w', L: 'y' };
+    [
+      // x' : M 回転
+      () => { [c.U, c.F, c.D, c.B] = [c.B, c.U, c.F, c.D]; },
+      // y' : E 回転
+      () => { [c.F, c.R, c.B, c.L] = [c.L, c.F, c.R, c.B]; },
+      // z' : S 回転
+      () => { [c.U, c.R, c.D, c.L] = [c.L, c.U, c.R, c.D]; },
+    ].forEach(func => {
+      const n = getRandomInt(3);
+      for (let i=0; i<n; i++) {
+        func();
+      }
+    });
+    const colors = Object.entries(c).map(([k, v]) => `${k}:${v}` ).join(' ');
+    return `|colors=${colors}`;
+  }
+}
+const cm = new ConfigMaker;
+cm.config();
+
+function scramble() {
+  if (CubeAnimation.webgl_cubes >= 16) {
+    location.reload();
+  }
+  Object.entries(CubeAnimation.by_id).forEach(([_, v]) => v.remove());
+  // class="roofpig" がないと、アニメーションや諸々の機能がなくなる
+  CubeAnimation.create_in_dom('#sunaba', cm.make(), 'class="roofpig"');
+}
+
+Array.from(document.getElementsByTagName('input')).forEach(input => {
+  // bindしないと、thisが input になる
+  input.addEventListener('change', cm.config.bind(cm));
+});
+document.getElementById('scramble').addEventListener('click', scramble);
+
+// keyboardイベントはCubeAnimetionに持っていかれる
+// document.addEventListener('keydown', e => {
+//   console.log(e.keyCode);
+// }, false);
+
+// deferで読み込まれるが、即実行するとエラー画面が出るので適当に時間を置く
+setTimeout(scramble, 50);
